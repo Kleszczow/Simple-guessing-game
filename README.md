56678fc2b52a5647145cacb6b5b6071a9fc26281
Tymek Slomski <tymekslomski@gmail.com>
2023-05-04 22:21:03 +0200

    major tasks completed

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Hello
 Hello World!
+
 This project is a short guess the number game. But that's not what I'm focusing on. Thanks to this repository, I want to learn git fully.
 
 Cheers Tymek
--- base.js
+++ base.js
@@ -1,15 +1,38 @@
 const inpValue = document.getElementById("inpValue");
 const submitBtn = document.getElementById("submitBtn");
 const numberShow = document.getElementById("numberShow");
+const win = document.getElementById("win");
+const lost = document.getElementById("lost");
 
 submitBtn.addEventListener("click", () => {
   checkValue();
 });
 
 const checkValue = () => {
-  if (inpValue.value >= 0 && inpValue.value <= 10) {
+  if (inpValue.value >= 0 && inpValue.value <= 10 && inpValue.value !== "") {
+    numberShow.innerHTML = aiChocie();
+    resoluts();
   } else {
     alert("Your guess is not on the scale pls try agin :/");
     inpValue.value = "";
   }
+};
+
+const aiChocie = (ai) => {
+  return Math.floor(Math.random() * 11);
+};
+
+const resoluts = () => {
+  let ai = aiChocie();
+  let player = inpValue.value;
+  let resolut = ai == player ? winFunction() : lostunction();
+};
+
+const winFunction = () => {
+  lost.style.display = "none";
+  win.style.display = "block";
+};
+const lostunction = () => {
+  win.style.display = "none";
+  lost.style.display = "block";
 };
